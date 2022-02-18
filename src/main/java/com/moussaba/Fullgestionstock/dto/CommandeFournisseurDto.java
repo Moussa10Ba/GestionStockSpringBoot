90dab528a20132b5445fab479661a0bf22dedc34
Moussa Ba <moussa10ba@gmail.com>
2022-02-18 01:44:54 +0000

    Seconde Dto commit

--- src/main/java/com/moussaba/Fullgestionstock/dto/CommandeFournisseurDto.java
+++ src/main/java/com/moussaba/Fullgestionstock/dto/CommandeFournisseurDto.java
@@ -1,4 +1,5 @@
 package com.moussaba.Fullgestionstock.dto;
+import com.moussaba.Fullgestionstock.model.CommandeFournisseur;
 import lombok.Builder;
 import lombok.Data;
 
@@ -20,8 +21,32 @@ public class CommandeFournisseurDto {
 
     private Integer idEntreprise;
 
+    public static CommandeFournisseurDto fromEntity(CommandeFournisseur commandeFournisseur) {
+        if (commandeFournisseur == null) {
+            return null;
+        }
+        return CommandeFournisseurDto.builder()
+                .id(commandeFournisseur.getId())
+                .code(commandeFournisseur.getCode())
+                .dateCommande(commandeFournisseur.getDateCommande())
+                .fournisseur(FournisseurDto.fromEntity(commandeFournisseur.getFournisseur()))
+               // .etatCommande(commandeFournisseur.getEtatCommande())
+                .idEntreprise(commandeFournisseur.getIdEntreprise())
+                .build();
+    }
 
-
-
+    public static CommandeFournisseur toEntity(CommandeFournisseurDto dto) {
+        if (dto == null) {
+            return null;
+        }
+        CommandeFournisseur commandeFournisseur = new CommandeFournisseur();
+        commandeFournisseur.setId(dto.getId());
+        commandeFournisseur.setCode(dto.getCode());
+        commandeFournisseur.setDateCommande(dto.getDateCommande());
+        commandeFournisseur.setFournisseur(FournisseurDto.toEntity(dto.getFournisseur()));
+        commandeFournisseur.setIdEntreprise(dto.getIdEntreprise());
+      //  commandeFournisseur.setEtatCommande(dto.getEtatCommande());
+        return commandeFournisseur;
+    }
 
 }
--- src/main/java/com/moussaba/Fullgestionstock/dto/FournisseurDto.java
+++ src/main/java/com/moussaba/Fullgestionstock/dto/FournisseurDto.java
@@ -2,6 +2,7 @@ package com.moussaba.Fullgestionstock.dto;
 
 import com.moussaba.Fullgestionstock.model.Adresse;
 import com.moussaba.Fullgestionstock.model.CommandeFournisseur;
+import com.moussaba.Fullgestionstock.model.Fournisseur;
 import lombok.Builder;
 import lombok.Data;
 
@@ -29,4 +30,39 @@ public class FournisseurDto {
     private String numTel;
 
     private List<CommandeFournisseurDto> commandeFournisseurs;
+
+    private Integer idEntreprise;
+
+    public static FournisseurDto fromEntity(Fournisseur fournisseur) {
+        if (fournisseur == null) {
+            return null;
+        }
+        return FournisseurDto.builder()
+                .id(fournisseur.getId())
+                .nom(fournisseur.getNom())
+                .prenom(fournisseur.getPrenom())
+                .adresse(AdresseDto.fromEntity(fournisseur.getAdresse()))
+                .photo(fournisseur.getPhoto())
+                .mail(fournisseur.getMail())
+                .numTel(fournisseur.getNumTel())
+                .idEntreprise(fournisseur.getIdEntreprise())
+                .build();
+    }
+
+    public static Fournisseur toEntity(FournisseurDto dto) {
+        if (dto == null) {
+            return null;
+        }
+        Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setId(dto.getId());
+        fournisseur.setNom(dto.getNom());
+        fournisseur.setPrenom(dto.getPrenom());
+        fournisseur.setAdresse(AdresseDto.toEntity(dto.getAdresse()));
+        fournisseur.setPhoto(dto.getPhoto());
+        fournisseur.setMail(dto.getMail());
+        fournisseur.setNumTel(dto.getNumTel());
+        fournisseur.setIdEntreprise(dto.getIdEntreprise());
+
+        return fournisseur;
+    }
 }
